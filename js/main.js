99da8a3fc70ae6742adf0b01d99a29d27d1e35a5
heronblade <bylerj@gmail.com>
2016-08-30 14:10:29 -0400

    got the click functions working

--- js/main.js
+++ js/main.js
@@ -3,48 +3,45 @@
 particlesJS.load('particles-js', 'data/particles.json', function() {});
 
 if (document.readyState != 'loading') {
-	console.log('yo');
 	docReady();
 } else {
 	document.addEventListener('DOMContentLoaded', docReady);
-	console.log('lll');
 }
 
+function scrollTo(element, to, duration) {
+	console.log('scrolling');
+	if (duration <= 0) return;
+	var difference = to - element.scrollTop;
+	var perTick = difference / duration * 10;
 
+	console.log('diff: ' + difference);
+	console.log('perTick: ' + perTick);
 
+	setTimeout(function() {
+		element.scrollTop = element.scrollTop + perTick;
+		if (element.scrollTop === to) return;
+		scrollTo(element, to, duration - 10);
+	}, 10);
+};
 
 function docReady() {
+	var aboutLink = document.getElementById('aboutLink');
+	var codeLink = document.getElementById('codeLink');
+	var contactLink = document.getElementById('contactLink');
+	var aboutSection = document.getElementById('about');
+	var codeSection = document.getElementById('code');
+	var contactSection = document.getElementById('contact');
 
-	console.log('doc ready');
+	aboutLink.addEventListener('click', function(){scrollTo(aboutLink,aboutSection, 100)}, false);
+	codeLink.addEventListener('click', function(){scrollTo(codeLink, codeSection, 100)});
+	contactLink.addEventListener('click', function(){scrollTo(contactLink, contactSection, 100)});
 
-	// var scrollTo = function(element, to, duration) {
-	// 	console.log('scrolling');
-	// 	if (duration <= 0) return;
-	// 	var difference = to - element.scrollTop;
-	// 	var perTick = difference / duration * 10;
-	//
-	// 	setTimeout(function() {
-	// 		element.scrollTop = element.scrollTop + perTick;
-	// 		if (element.scrollTop === to) return;
-	// 		scrollTo(element, to, duration - 10);
-	// 	}, 10);
-	// };
-	//
-	// var aboutLink = document.getElementById('aboutLink');
-	// var codeLink = document.getElementById('codeLink');
-	// var contactLink = document.getElementById('contactLink');
-	// var aboutSection = document.getElementById('about');
-	// var codeSection = document.getElementById('code');
-	// var contactSection = document.getElementById('contact');
-	//
-	// aboutLink.addEventListener('click', scrollTo(aboutLink, aboutSection, 100));
-	// codeLink.addEventListener('click', scrollTo(codeLink, codeSection, 100));
-	// contactLink.addEventListener('click', scrollTo(contactLink, contactSection, 100));
-	//
-	// aboutLink.addEventListener('touch', scrollTo(aboutLink, aboutSection, 100));
-	// codeLink.addEventListener('touch', scrollTo(codeLink, codeSection, 100));
-	// contactLink.addEventListener('touch', scrollTo(contactLink, contactSection, 100));
+	aboutLink.addEventListener('touch', function(){scrollTo(aboutLink, aboutSection, 100)});
+	codeLink.addEventListener('touch', function(){scrollTo(codeLink, codeSection, 100)});
+	contactLink.addEventListener('touch', function(){scrollTo(contactLink, contactSection, 100)});
 };
+
+
 
 
 
